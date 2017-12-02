3148f0cc613f9b928a89fa72ca3e364baa49b575
Silvan Vella <Silvan Vella>
2017-12-02 11:22:21 +0100

    - Added a background - Added a ball and 2 paddle (player 1, player 2) objects - Changed the screen resolution - Created 2 scripts to make the paddles movable, one by keyboard the other by mouse

---
+++ Ball Game/Assets/Scripts/Player_1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_1 : MonoBehaviour {
+
+    private Rigidbody2D myRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 newPlayer1Pos;
+
+        if (Input.GetKey("up"))
+        {
+            newPlayer1Pos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.3f, gameObject.transform.position.z);
+            gameObject.transform.position = newPlayer1Pos;
+        }
+
+        if (Input.GetKey("down"))
+        {
+            newPlayer1Pos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 0.3f, gameObject.transform.position.z);
+            gameObject.transform.position = newPlayer1Pos;
+        }
+
+        
+
+    }
+
+}
---
+++ Ball Game/Assets/Scripts/Player_2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float mousePosInUnits = (Input.mousePosition.y / Screen.width * 20) - 5;
+
+
+        Vector3 newPaddlePos = new Vector3(gameObject.transform.position.x, mousePosInUnits, gameObject.transform.position.z);
+
+        newPaddlePos.y = Mathf.Clamp(mousePosInUnits, -3.99f, 3.99f);
+
+        gameObject.transform.position = newPaddlePos;
+    }
+}
