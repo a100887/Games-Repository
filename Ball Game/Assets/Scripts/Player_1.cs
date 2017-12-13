167f171150300e47ce4825d8a4f2ba2d1f23105f
Silvan Vella <Silvan Vella>
2017-12-13 20:55:14 +0100

    - Clamped the player 1 (paddle) on the y-axis so it would not exit the game's borders

--- Ball Game/Assets/Scripts/Player_1.cs
+++ Ball Game/Assets/Scripts/Player_1.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class Player_1 : MonoBehaviour {
 
+    float yPos = 0f;
+
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-        Vector3 newPlayer1Pos;
-
+        
         if (Input.GetKey("up"))
         {
-            newPlayer1Pos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.3f, gameObject.transform.position.z);
-            gameObject.transform.position = newPlayer1Pos;
+            yPos += 0.3f;
         }
 
         if (Input.GetKey("down"))
         {
-            newPlayer1Pos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 0.3f, gameObject.transform.position.z);
-            gameObject.transform.position = newPlayer1Pos;
+            yPos -= 0.3f;
         }
+
+        gameObject.transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
+        yPos = Mathf.Clamp(gameObject.transform.position.y, -3.99f, 3.99f);
     }
 }
